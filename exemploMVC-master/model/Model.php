13787790f941e8285428576948171c870682f25a
otaviovdavila <otavio.davila23@gmail.com>
2019-12-12 20:04:37 -0300

    Finalera msm

--- exemploMVC-master/model/Model.php
+++ exemploMVC-master/model/Model.php
@@ -6,8 +6,6 @@ class Model {
         if (isset($_POST['username']) && isset($_POST['password'])) {
             if ($_POST['username'] == 'admin' &&
                     $_POST['password'] == 'admin') {
-                $_SESSION['username'] = $_POST['username'];
-                $_SESSION['password'] = $_POST['password'];
                 return 'login';
             } else {
                 return 'usuario invalido';
